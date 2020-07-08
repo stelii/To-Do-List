4ef2cc374138613b90ce946329f0ec515531d8b6
Fedorca <steli.fedorca25@gmail.com>
2020-07-08 11:47:41 +0300

    I made the status bar to be transparent

--- app/src/main/java/my/projects/todolist/MainActivity.java
+++ app/src/main/java/my/projects/todolist/MainActivity.java
@@ -1,6 +1,7 @@
 package my.projects.todolist;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.navigation.NavController;
 import androidx.navigation.NavGraph;
 import androidx.navigation.Navigation;
@@ -14,7 +15,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        setUpToolbar();
+    }
 
+    private void setUpToolbar(){
+        Toolbar toolbar = findViewById(R.id.toolbar);
+//        toolbar.setTitle("");
+        setSupportActionBar(toolbar);
     }
 
 //    private void navigateConditionally(){
